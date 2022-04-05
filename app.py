65a0bc4815b9f048f515bad3bf5f907367a035c9
Mame Coumba Ka <mck@dhcp-10-105-94-232.wireless.northwestern.private>
2022-04-04 23:42:42 -0500

    update

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
 from twilio.rest import Client
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+project_dir = os.path.dirname(os.path.abspath(__file__))
+
 class _node:
     def __init__(self, data):
         self.data = data
@@ -55,10 +59,11 @@ class myqueue () :
         
 
 class person : 
-    def __init__ (self ,id, name ,  phone) :
+    def __init__ (self ,id, name ,  phone , sms_content) :
         self.id = id
         self.name = name
         self.phone = phone
+        self.sms = sms_content
         
 class sms_system :
     #starts by initializing a dictionary of queues stored with the type as key 
@@ -66,33 +71,52 @@ class sms_system :
         self.queue = myqueue()
         
     
-    def add_person(self,name, id, phone , request ) :
-        patron_ = patron (id ,phone , request)
-        self.queues[request].enqueue(patron_)
+    def add_person(self, id , name , phone , sms)   :
+        they = person(id, name, phone , sms )
+        self.queue.enqueue( they)
             
-    def send_sms(self, patron_) :
+    def send_sms(self, person ) :
         account_sid = "AC8fc71d27854fc7587d71c70b421610d5"
-        auth_token = "44060ae9b4ae6e33964c0701b6f41dbd"
+        auth_token = "c743187bbc93a749cec98ab3c1d506f6"
         client = Client(account_sid, auth_token)
         message = client.messages.create(
-            to=patron_.phone,
-            from_='+17692468375',
-            body='Your item is ready for pickup at the library desk')
+            to=person.phone,
+            from_='+18787897833',
+            body= person.sms)
         print(message.sid)
 
     def update_queue (self) :
-          try
+          try :
                 next_patron = self.queue.dequeue()
                 self.send_sms(next_patron)
-            except :
-                print ('no patrons on the waitlist')
+
+          except : 
+                print ('queue is empty')
+    def queue_to_list (self) :
+        list_of_people = []
+        current_node = self.queue.head
+        while current_node != None :
+            list_of_people.append(current_node.data)
+            current_node = current_node.next
+        return list_of_people
+         
                 
-    
+@app.route("/" , methods=["GET", "POST"])
+
+def view_index() :
+    if request.method == "POST" :
+       sms.add_person(request.form["id"], request.form["name"], request.form["phone"] , request.form["sms_content"])
+    elif request.method == "GET" :
+        sms.update_queue()
+    return render_template("index.html", queue=sms.queue_to_list())
         
                 
 
 
 if __name__ == "__main__":
+    
+    sms = sms_system()
+    app.run(debug=True)
     
         
     
